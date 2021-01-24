b7859366ae411ebbef519280dccbd471f511d1ee
Denna Bellman <Denna@Dennas-MacBook.local>
2021-01-23 21:19:53 -0500

    Added api functinality, as well as getting users coordinates.

---
+++ weather.js
@@ -0,0 +1,50 @@
+//Define variables
+let latitude;
+let longitude;
+let apiKey = "cf1061f2db824499aa730543211401";
+
+//elements on html page
+let weatherTempHeader = document.querySelector('.weather');
+let weatherDetails = document.querySelector('.weather-details');
+
+//Get the coordinates of the user 
+window.addEventListener('load', () => {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+            longitude = position.coords.longitude;
+            latitude = position.coords.latitude;
+            console.log(longitude);
+            console.log(latitude);
+        });
+    } else {
+        weather_temp.innerText = "Please enable your location for the weather to display";
+    }
+});
+
+function resolveAfterTwoseconds() {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve('Resolved');
+        }, 1000);
+    });
+}
+
+//getting weather forecast data
+async function asyncCall() {
+    // console.log("Calling...");
+    const result = await resolveAfterTwoseconds();
+    fetch(`https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${latitude},${longitude}&days=7`)
+    .then(respone => respone.json())
+    .then(data => {
+        console.log(data);
+
+        //Display elements and create elements needed
+        weatherTempHeader.innerText = `${data.current.temp_f} F`;
+        
+
+    });
+}
+
+asyncCall();
+
+//const result = await resolveAfter2Seconds();
